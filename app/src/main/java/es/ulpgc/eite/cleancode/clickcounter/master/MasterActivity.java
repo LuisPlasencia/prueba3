805629108da95d35165fb6bd7fef9e7b900c4bf0
Luis Plasencia <luisplasenciapulido@gmail.com>
2020-05-13 00:06:08 +0100

    d

---
+++ app/src/main/java/es/ulpgc/eite/cleancode/clickcounter/master/MasterActivity.java
@@ -0,0 +1,105 @@
+package es.ulpgc.eite.cleancode.clickcounter.master;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import es.ulpgc.eite.cleancode.clickcounter.R;
+import es.ulpgc.eite.cleancode.clickcounter.app.AppMediator;
+import es.ulpgc.eite.cleancode.clickcounter.data.CounterData;
+
+public class MasterActivity
+    extends AppCompatActivity implements MasterContract.View {
+
+  public static String TAG = MasterActivity.class.getSimpleName();
+
+  private MasterContract.Presenter presenter;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_master);
+    getSupportActionBar().setTitle(R.string.app_name);
+
+    if (savedInstanceState == null) {
+      AppMediator.resetInstance();
+    }
+
+    // do the setup
+    MasterScreen.configure(this);
+
+    if (savedInstanceState == null) {
+      presenter.onStart();
+
+    } else {
+      presenter.onRestart();
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    // load the data
+    presenter.onResume();
+  }
+
+  @Override
+  public void onBackPressed() {
+    super.onBackPressed();
+
+    presenter.onBackPressed();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+
+    presenter.onPause();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+
+    presenter.onDestroy();
+  }
+
+
+  public void onButtonPressed(View view) {
+    presenter.onButtonPressed();
+  }
+
+
+  @Override
+  public void onDataUpdated(MasterViewModel viewModel) {
+    //Log.e(TAG, "onDataUpdated()");
+
+    // deal with the datasource
+    ((ListView) findViewById(R.id.list)).setAdapter(new MasterAdapter(
+            this, viewModel.datasource, new View.OnClickListener() {
+
+          @Override
+          public void onClick(View view) {
+            CounterData data = (CounterData) view.getTag();
+
+          }
+        })
+    );
+  }
+
+  @Override
+  public void navigateToNextScreen() {
+    Intent intent = new Intent(this, MasterActivity.class);
+    startActivity(intent);
+  }
+
+
+  @Override
+  public void injectPresenter(MasterContract.Presenter presenter) {
+    this.presenter = presenter;
+  }
+}
---
+++ app/src/main/java/es/ulpgc/eite/cleancode/clickcounter/master/MasterContract.java
@@ -0,0 +1,59 @@
+package es.ulpgc.eite.cleancode.clickcounter.master;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+import es.ulpgc.eite.cleancode.clickcounter.app.DetailToMasterState;
+import es.ulpgc.eite.cleancode.clickcounter.app.MasterToDetailState;
+import es.ulpgc.eite.cleancode.clickcounter.data.CounterData;
+
+public interface MasterContract {
+
+  interface View {
+    void injectPresenter(Presenter presenter);
+
+    void onDataUpdated(MasterViewModel viewModel);
+    void navigateToNextScreen();
+
+  }
+
+  interface Presenter {
+    void injectView(WeakReference<View> view);
+
+    void injectModel(Model model);
+
+    void injectRouter(Router router);
+
+    void onResume();
+
+    void onStart();
+
+    void onRestart();
+
+    void onBackPressed();
+
+    void onPause();
+
+    void onDestroy();
+
+    void onButtonPressed();
+  }
+
+  interface Model {
+    List<CounterData> getStoredData();
+
+    void onDataFromNextScreen(String data);
+
+    void onRestartScreen(List<CounterData> datasource);
+
+    void onDataFromPreviousScreen(String data);
+  }
+
+  interface Router {
+
+    void passStateToNextScreen(MasterToDetailState state);
+
+    DetailToMasterState getStateFromNextScreen();
+
+  }
+}
