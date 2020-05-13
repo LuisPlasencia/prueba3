1bd6acd72d90ebf2865097dafe2ce7b393c66aa2
Luis Plasencia <luisplasenciapulido@gmail.com>
2020-05-13 14:58:19 +0100

    solucion

--- app/src/main/java/es/ulpgc/eite/cleancode/clickcounter/app/AppMediator.java
+++ app/src/main/java/es/ulpgc/eite/cleancode/clickcounter/app/AppMediator.java
@@ -1,62 +1,58 @@
 package es.ulpgc.eite.cleancode.clickcounter.app;
 
-import android.app.Application;
-
 import es.ulpgc.eite.cleancode.clickcounter.detail.DetailState;
 import es.ulpgc.eite.cleancode.clickcounter.master.MasterState;
 
-public class AppMediator extends Application {
+public class AppMediator  {
 
-  private static AppMediator instance;
+    private static AppMediator instance;
 
-  private MasterState masterState;
-  private DetailState detailState;
+    private MasterState masterState;
+    private DetailState detailState;
 
-  private DetailToMasterState detailToMasterState;
-  private MasterToDetailState masterToDetailState;
+    private DetailToMasterState detailToMasterState;
+    private MasterToDetailState masterToDetailState;
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    detailState= new DetailState();
-    masterState= new MasterState();
+    private AppMediator() {
+        detailState= new DetailState();
+        masterState= new MasterState();
 
-  }
-
-  public static AppMediator getInstance() {
-    if(instance == null){
-      instance = new AppMediator();
     }
 
-    return instance;
-  }
+    public static AppMediator getInstance() {
+        if(instance == null){
+            instance = new AppMediator();
+        }
 
-  public static void resetInstance() {
-    instance=null;
-  }
+        return instance;
+    }
 
-  public MasterState getMasterState() {
-    return masterState;
-  }
+    public static void resetInstance() {
+        instance=null;
+    }
+
+    public MasterState getMasterState() {
+        return masterState;
+    }
 
 
-  public DetailState getDetailState() {
-    return detailState;
-  }
+    public DetailState getDetailState() {
+        return detailState;
+    }
 
-  public void setPreviousDetailScreenState(DetailToMasterState state) {
-    detailToMasterState=state;
-  }
+    public void setPreviousDetailScreenState(DetailToMasterState state) {
+        detailToMasterState=state;
+    }
 
-  public MasterToDetailState getPreviousDetailScreenState() {
-    return masterToDetailState;
-  }
+    public MasterToDetailState getPreviousDetailScreenState() {
+        return masterToDetailState;
+    }
 
-  public void setNextMasterScreenState(MasterToDetailState state) {
-    masterToDetailState=state;
-  }
+    public void setNextMasterScreenState(MasterToDetailState state) {
+        masterToDetailState=state;
+    }
 
-  public DetailToMasterState getNextMasterScreenState() {
-    return detailToMasterState;
-  }
+    public DetailToMasterState getNextMasterScreenState() {
+        return detailToMasterState;
+    }
 }
